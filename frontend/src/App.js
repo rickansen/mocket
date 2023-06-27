d460520cdaa2a8344dc83eb1204937d38d253007
Andersen <rickansenn@gmail.com>
2023-06-28 00:14:05 +0800

    Define-Product-Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         </header>
         <main>
           <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
             <Route path="/" element={<HomeScreen />} />
           </Routes>
         </main>
