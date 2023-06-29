7fdbf41715da2f607c06693516ad95ccf3265ed7
Andersen <rickansenn@gmail.com>
2023-06-29 23:53:30 +0800

    Add-Helmet-Async

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,6 +4,7 @@ import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
+import { Helmet } from 'react-helmet-async';
 // import data from '../data';
 
 const reducer = (state, action) => {
@@ -44,6 +45,10 @@ function HomeScreen() {
   }, []);
   return (
     <div>
+      <Helmet>
+        <title>Mocket</title>
+      </Helmet>
+
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
