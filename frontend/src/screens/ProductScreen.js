e41274a5293475a35c25d6fd16d0bc1de1a91ca3
Andersen <rickansenn@gmail.com>
2023-07-01 00:09:13 +0800

    Implement-LoadingBox-And-MessageBox-Component

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -12,6 +12,7 @@ import ListGroupItem from 'react-bootstrap/esm/ListGroupItem';
 import { Helmet } from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { getError } from '../utils';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -43,7 +44,7 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: error.message });
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
       }
     };
     fetchData();
