0df261266e5893a726fdb979464e1aeeaf77d46c
Andersen <rickansenn@gmail.com>
2023-07-05 08:14:42 +0800

    Refactor-Spelling-Edit

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -58,7 +58,7 @@ function ProductScreen() {
   const addToCartHandler = async () => {
     const existItem = cart.cartItems.find((x) => x._id === product._id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
-    const { data } = await axios.get(`/api/product/${product._id}`);
+    const { data } = await axios.get(`/api/products/${product._id}`);
 
     if (data.countInStock < quantity) {
       window.alert('Sorry, Product is out of stock');
